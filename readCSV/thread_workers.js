a4a017ca41579a483e5fb1e22163a79cdd956e90
Mohit Tambi <mohit.tambi@hdworks.in>
2023-04-08 22:03:47 +0530

    check added

--- readCSV/thread_workers.js
+++ readCSV/thread_workers.js
@@ -1,23 +1,27 @@
 const { workerData, parentPort } = require("worker_threads");
 const { updateOne } = require('@hdworks/dynamo-db-wrapper')
 
-let counter = 0;
 console.log("IN thread workers")
+let counter = 0;
 let response;
 !!workerData.csvData && console.log("workerData.csvData.length", workerData.csvData.length)
 
 for (let i = 0; i < workerData.csvData.length; i++) {
 	counter++;
 	console.log("workerData.csvData._id", workerData.csvData._id)
-	response = updateOne('users',
-		{ _id: workerData.csvData._id },
-		{
-			$set: { mobileVerified: false }
-		}
-	)
+	try {
+		updateOne('users',
+			{ _id: workerData.csvData._id },
+			{
+				$set: { mobileVerified: false }
+			}
+		)
+	} catch (e) {
+		console.log("users error", e)
+	}
 	//console.log("responseresponseresponse", response)
 }
-//console.log("WORKER LAST DATA", workerData.csvData.slice(-1))
+console.log("WORKER LAST DATA", JSON.stringify(workerData.csvData.slice(-1)))
 
 //console.log("----parentPort", parentPort);
 
